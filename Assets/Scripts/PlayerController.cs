3851242b7bd1135041d59d37a0b68843bbe67f3d
Cursed <AmrodV2@gmail.com>
2023-01-28 20:42:26 +0100

    Poprawione strzelanie, zdrowie, pare prefabow. Reszta pozniej

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,11 @@ public class PlayerController : MonoBehaviour
 
     public Animator gunAnimation;
 
+    public int currentHealth;
+    public int maxHealth = 100;
+    public GameObject deadScreen;
+    public bool hasDied;
+
     public void Awake() {
 
         instance = this;
@@ -32,52 +37,82 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         
+        currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
     void Update() {
-        //poruszanie
-        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        Vector3 moveHorizontal = transform.up * -moveInput.x;
-        Vector3 moveVertical = transform.right * moveInput.y;
+        if(!hasDied) {
+            //poruszanie
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            Vector3 moveHorizontal = transform.up * -moveInput.x;
+            Vector3 moveVertical = transform.right * moveInput.y;
 
 
-        theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+            theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
 
-        //kamera
-        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
+            //kamera
+            mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
 
-       //strzelanie
+            //strzelanie
 
-       if(Input.GetMouseButton(0))
-       {
+            if(Input.GetMouseButtonDown(0)) {
 
-        if(currentAmmo > 0) {
+                if(currentAmmo > 0) {
 
-        
-            Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
-            {
-                //Debug.Log("Patrzę na " + hit.transform.name);
-                Instantiate(bulletImpact, hit.point, transform.rotation);
+                
+                    Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+                    RaycastHit hit;
+                    if(Physics.Raycast(ray, out hit))
+                    {
+                        //Debug.Log("Patrzę na " + hit.transform.name);
+                        Instantiate(bulletImpact, hit.point, transform.rotation);
 
-                if(hit.transform.tag == "Enemy") {
+                        if(hit.transform.tag == "Enemy") {
 
-                    hit.transform.parent.GetComponent<EnemyController>().TakeDamage();
+                            hit.transform.parent.GetComponent<EnemyController>().TakeDamage();
 
+                        }
+
+                    } else {
+                        Debug.Log("Patrzę na nic!");
+                    }
                 }
+                currentAmmo--;
+                gunAnimation.SetTrigger("Shoot");
 
-            } else {
-                Debug.Log("Patrzę na nic!");
             }
         }
-        currentAmmo--;
-        gunAnimation.SetTrigger("Shoot");
-       }
+
+    }
+
+    public void TakeDamage(int damageAmount) {
+
+        currentHealth -= damageAmount;
+
+        if( currentHealth <= 0) {
+
+            deadScreen.SetActive(true);
+            hasDied = true;
+
+        }
+
+    }
+
+    public void AddHealth(int healAmount) {
+
+        currentHealth =+ healAmount;
+
+        if(currentHealth > maxHealth) {
+
+            currentHealth = maxHealth;
+
+        }
 
     }
 }
